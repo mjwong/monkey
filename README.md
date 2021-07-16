690743b6cfc6c3634da34c868f6b823189d2281c
Michael Wong <mjwong6868@gmail.com>
2021-07-17 01:02:11 +0800

    Parse prefix expressions added

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
 # monkey
+Intrepretor for the Monkey programming language. 
+
+In the Monkey programming language, everything besides let and return statements is an expression, i.e an expression alone can be a statement itself. The language features are as follows:
+
+- C-like syntax
+- variable bindings
+- integers and booleans
+- arithmetic expressions
+- built-in functions
+- first-class and higher-order functions
+- closures
+- a string data structure
+- an array data structure
+- a hash data structure
+- operator precedence, infix/prefix operators
+
+E.g.
+
+Binding values:
+
+```
+let age = 1;
+let name = "Monkey";
+let result = 10 * (20 / 2);
+```
+
+Binding an array of integers:
+
+```
+let myArray = {1, 2, 3, 4, 5};
+```
+
+Binding a hash:
+
+```
+let me = {"name": "Michael", "age": 21};
+```
+
+Accessing elements in arrays and hashes:
+
+```
+myArray[0]	// ==> 1
+me["name"] 	// ==> "Michael"
+```
+
+Binding functions:
+
+Function literals as expression:
+
+```
+let add = fn(a, b) { return a + b; };
+```
+
+Function literal in place of an identifier:
+
+```
+fn(x, y) { return x + y}(5, 5)
+(fn(x) { return x}(5) * 10
+```
+
+If expressions in function literals:
+
+```
+let result = if (10>5)n { true } else { false };
+```
+result // => true
+
+Implicit return values (i.e. value of the last statement/expression in function is returned):
+
+```
+let add = fn(a, b) { a + b; };
+```
+
+Calling function:
+
+```
+add(1, 2);
+```
+
+Recursive calls to function (tail recursion):
+
+```
+let fibonacci = fn(x) {
+	if (x == 0) {
+		0
+	} else {
+		if (x == 1) {
+			1
+		} else {
+			fibonacci(x - 1) + fibonacci(x - 2);
+		}
+	}
+};
+```
+
+Binging higher order functions:
+
+```
+let twice = fn(f, x) {
+	return f(f(x));
+};
+
+let addTwo = fn(x) {
+	return x + 2;
+};
+
+twice(addTwo, 2); 	// ==> 6
+```
+
+Operator precedence:
+
+The validity of a token's position depends on the context the tokens that come before and after, and their precedence.
+
+
+Top Down Operator Precedence (PRATT) Parsing methodology is used to parse Expressions.
+
+The PRATT paraser's approach is to associate the parsing function with token types.
+Whenever this token type is encountered, the parsing functions are called to parse the appropriate expression and return an AST node that represents it.
+Each token type n have up to two parsing functions associated with it, depending on whether the token if found in a prefix o4 qn infix position.
+
+Major parts:
+
+- Lexer
+- Parser
+- Abstract Syntax Tree (AST)
+- Internal object system
+- Evaluator
--- parser/parser.go
+++ parser/parser.go
@@ -2,10 +2,22 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/mjwong/monkey/ast"
 	"github.com/mjwong/monkey/lexer"
 	"github.com/mjwong/monkey/token"
+)
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X OR !X
+	CALL        // myFunction(X)
 )
 
 type Parser struct {
@@ -14,6 +26,9 @@ type Parser struct {
 	curToken  token.Token
 	peekToken token.Token
 	errors    []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -21,6 +36,12 @@ func New(l *lexer.Lexer) *Parser {
 		l:      l,
 		errors: []string{},
 	}
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	// Read two tokens, so curToken and peetToken are both set
 	p.nextToken()
@@ -56,7 +77,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -122,4 +143,81 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+// Parser helper functions
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	// semicoln in expressions are optional, ie. can type 5 + 5 into the REPL
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+
+	// check whether there is a parsing function associated with the token type
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
